7a767aeda8c39634b003cdce3a4cd4c28efecf13
heavyinthegame <wedd0730@icloud.com>
2025-12-25 11:31:29 +0900

    Initial Commit

---
+++ tex180/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tex180
+//
+//  Created by 幸川 on 2025/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tex180/tex180App.swift
@@ -0,0 +1,17 @@
+//
+//  tex180App.swift
+//  tex180
+//
+//  Created by 幸川 on 2025/12/25.
+//
+
+import SwiftUI
+
+@main
+struct tex180App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
